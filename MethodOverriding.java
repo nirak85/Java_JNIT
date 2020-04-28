6589995bd90facd94fdc0fc7c1c8911710cd9328
nirak85 <niraks85@gmail.com>
2020-04-28 00:05:38 -0500

    Add files via upload

--- MethodOverriding.java
+++ MethodOverriding.java
@@ -1,41 +1,35 @@
-//Method overriding in Java programming occurs when the method in the subclass has the same return type, or parameters, name or signature as the parent class. 
-//Method overriding is the method by which Java can support runtime polymorphism.
-
-class Fruit{
-	void protein(){
-		System.out.println("No protein");
-	}
-	void vitamin(){
-		System.out.println("No vitamin");
-	}
+abstract class Laptop{
+abstract void screenTouch();
+abstract void cost();
+void keyboard(){
+System.out.println("All laptop has keyboard on it");
 }
-class Pineapple extends Fruit{
-	void protein(){
-		System.out.println("Pineapple is not a significant source of protein.");
-	}
-	void vitamin(){
-		System.out.println("Pineapple is rich in vitaminC");
-	}
 }
-class Guava extends Fruit{
-	void protein(){
-		System.out.println("Guava have high level of protein");
-	}
-	void vitamin(){
-		System.out.println("Guava contains of vitaminC, potassium and fibre");
-	}
+class Lenovo extends Laptop{
+void screenTouch(){
+System.out.println("Lenovo have screen touch display");
 }
-public class MethodOverriding {
-	public static void main(String[] args) {
-		Fruit f= new Fruit();
-		f.protein();
-		f.vitamin();
-		f=new Pineapple();
-		f.protein();
-		f.vitamin();
-		f=new Guava();
-		f.protein();
-		f.vitamin();
-	}
-
+void cost(){
+  System.out.println("The cost of Lenevo laptop is cheap");
+}
+}
+class MacBook extends Laptop{
+void screenTouch(){
+System.out.println("MacBook have not screen touch display");
+}
+void cost(){
+  System.out.println("The cost of Macbook is more than the Lenovo");
+}
+}
+class MethodOverriding{
+public static void main(String args[]){
+Laptop obj= new Lenovo();
+obj.keyboard();
+obj.screenTouch();
+obj.cost();
+obj= new MacBook();
+obj.keyboard();
+obj.screenTouch();
+obj.cost();
+}
 }
